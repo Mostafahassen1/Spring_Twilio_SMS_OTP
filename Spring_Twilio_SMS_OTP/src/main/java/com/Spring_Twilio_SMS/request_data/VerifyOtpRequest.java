df5c6b881e1f224502a21eecb1824bc5e6b721f1
Mostafa Hussien <mostafahass314@gmail.com>
2024-07-17 17:03:12 +0300

    added Docker-compos/ redis

---
+++ Spring_Twilio_SMS_OTP/src/main/java/com/Spring_Twilio_SMS/request_data/VerifyOtpRequest.java
@@ -0,0 +1,4 @@
+package com.Spring_Twilio_SMS.request_data;
+
+public class VerifyOtpRequest {
+}
---
+++ Spring_Twilio_SMS_OTP/src/main/java/com/Spring_Twilio_SMS/sender/TwilioSenderVerifyOtp.java
@@ -0,0 +1,4 @@
+package com.Spring_Twilio_SMS.sender;
+
+public interface TwilioSenderVerifyOtp {
+}
